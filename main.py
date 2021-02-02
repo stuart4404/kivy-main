7610c20841978843f6f82f2ec60b54ef6ae746d0
stuart tichener <76698984+stuart4404@users.noreply.github.com>
2021-02-02 21:48:24 +0000

    kivy app updated

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
-from kivy.properties import StringProperty, NumericProperty
+from kivy.properties import StringProperty, NumericProperty, ListProperty
 from kivy.core.window import Window
 from kivy.uix.popup import Popup
 from kivy.uix.image import Image
 from kivy.uix.modalview import ModalView
+from random import randint
 
 Window.clearcolor = (1,1,1,1)
 Window.size = (400,600)
@@ -20,12 +21,64 @@ class Progressbar(Widget):
 
 
 class ImageBox(Widget):
-    pass
+    index = NumericProperty(0)
+    image_name = StringProperty("")
+    
+    def get_image_path(self,image_name):
+        return "images/" + image_name + ".png"
+
 
 class GameScreen(Widget):
+    correct_answer_index = NumericProperty(0)
+    display_image_names = ListProperty([
+        "bote.png",
+        "diamante",
+        "espada",
+        "hacha",
+        ])
+
+    correct_answer_text = StringProperty("bote")
+    
+    
+    item_names = [
+        "bote.png",
+        "diamante",
+        "espada",
+        "hacha",
+        "ladrillos",
+        "manzana",
+        "pala",
+        "pasto",
+        "roca",
+        "tierra"
+    ]
+
+
+    def __init__(self,**kwargs):
+        super(GameScreen,self).__init__(**kwargs)
+        self.load_new_question()
+    
+    
+    def get_random_image_names(self):
+        temp_item_names = self.item_names.copy()
+        items = []
+        for _ in range(4):
+            rand_index = randint(0,len(temp_item_names)-1)
+            items.append(temp_item_names[rand_index])
+            del temp_item_names[rand_index]
+        return items
+
+
+    def load_new_question(self):
+        self.display_image_names = self.get_random_image_names
+        self.correct_answer_index = randint(0,3)
+        self.correct_answer_index = self.display_image_names[self.rand_index]    
+
     
     def show_end_game_popup(self):
-        end_game_screen = EndGameScreen(size_hint= (.75,.6),auto_dismiss = False)
+        end_game_screen = EndGameScreen(
+            size_hint= (.75,.6),
+            auto_dismiss = False)
         end_game_screen.open()
         
     
